2acb31eb60187343024878abafd882ea5dd9e77e
jesusVazquez21 <jesus_3141240253@utd.edu.mx>
2026-02-03 18:10:30 -0600

    actu4

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Cara} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://utd.edu.mx/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mi primera App en React
-        </a>
+        <img src={Cara} className="App-avatar" alt="logo" />
+        <h1 className="App-title">Evaluación parcial 1</h1>
+        <h2 className="App-student">Alumno(a): Jesus Vazquez Hernandez</h2>
+        
+        <div className="App-links-container">
+          <a className="App-link" href="https://www.linkedin.com/in/jesus-vazquez-hernandez-14bb793a4/" target="_blank" rel="noreferrer">
+            LINKED IN DE MI PERFIL
+          </a>
+          {/* ENLACE AL NUEVO HTML */}
+          <h1> </h1>
+          <a className="App-link" href="/documentacion.html">
+            DOCUMENTACION PARCIAL 1
+          </a>
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Calculadora from './calculadora';
+import Componente from './componente';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
